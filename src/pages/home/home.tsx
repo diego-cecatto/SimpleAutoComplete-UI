3ea5baeebb91dbb0ecb5e2c0262f0ed3c827c54b
Diego Cecatto <diego.cecatto@hotmail.com>
2024-10-01 08:28:51 -0300

    init: create structure

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { Autocomplete, Container } from '@mui/material';
+
+export const HomePage = () => {
+    return (
+        <Container sx={{ p: 3 }} maxWidth="xl">
+            Wellcome
+        </Container>
+    );
+};
